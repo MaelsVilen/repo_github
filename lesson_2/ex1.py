bd7fd202b3aec3b74a966ff06dc672eaa0a26d2c
MaelsVilen <maels66@gmail.com>
2020-04-05 17:06:19 +0300

    lesson 2 exercises added

---
+++ lesson_2/ex1.py
@@ -0,0 +1,4 @@
+ex1_list = [1.86, 42, 4 - 7j, "good_day_mr.Freeman", (1, 6, 45), None, {3, 7, 9, 21}, b"x01"]
+for i in ex1_list:
+    print(f"Тип объекта с индексом {ex1_list.index(i)} - {type(i)}")
+
