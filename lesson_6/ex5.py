4b76685807a6a189358fd598b15b8c6cd6396f90
MaelsVilen <maels66@gmail.com>
2020-04-21 01:55:20 +0300

    objects added for ex5

--- lesson_6/ex5.py
+++ lesson_6/ex5.py
@@ -28,3 +28,12 @@ class Handle(Stationery):
 
     def draw(self):
         print('Выделяю полезную информацию')
+
+p1 = Pen('Parker')
+p1.draw()
+
+p2 = Pencil('H2')
+p2.draw()
+
+p3 = Handle('Highlighter')
+p3.draw()
